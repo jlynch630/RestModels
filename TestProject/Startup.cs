09964882e23fbdfefe6eb056987cd428e57926e2
jlynch630 <jlynch630@gmail.com>
2020-06-03 08:46:10 -0700

    Add test to src ctrl

---
+++ TestProject/Startup.cs
@@ -0,0 +1,103 @@
+// -----------------------------------------------------------------------
+// <copyright file="Startup.cs" company="John Lynch">
+//   This file is licensed under the MIT license
+//   Copyright (c) 2020 John Lynch
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TestProject {
+	using System;
+	using System.Diagnostics;
+	using System.Linq;
+
+	using Microsoft.AspNetCore.Builder;
+	using Microsoft.AspNetCore.Hosting;
+	using Microsoft.AspNetCore.Http;
+	using Microsoft.EntityFrameworkCore;
+	using Microsoft.Extensions.DependencyInjection;
+	using Microsoft.Extensions.Hosting;
+
+	using RestModels.Auth;
+	using RestModels.EntityFramework;
+	using RestModels.ExceptionHandlers;
+	using RestModels.Exceptions;
+	using RestModels.Extensions;
+	using RestModels.Filters;
+	using RestModels.EntityFramework.Filters;
+	using RestModels.Models;
+	using RestModels.EntityFramework.Operations;
+	using RestModels.Parsers;
+	using RestModels.Results;
+
+	using TestProject.TestComponents;
+	using RestModels.EntityFramework.Extensions;
+	using RestModels.Responses;
+
+	public class Startup {
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+			if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+			app.UseRouting();
+			app.UseEndpoints(
+				endpoints => {
+					endpoints.MapGet("/a", async context => { await context.Response.WriteAsync("Hello World!"); });
+				});
+			app.UseEntityFrameworkRestModels<TestModel, TestDbContext>(
+				"/v1", 
+				options => {
+					// Create: /
+					// Read: /
+					// Read: /{id}
+					// Update: /{id}
+					// Delete: /{id}
+					/*
+					options.WriteJsonOrXml()
+						.StripArrayIfSingleResult()
+						.CatchExceptions()
+						.ParseXmlAndJsonArrays()
+						.AuthQuery("key", "john")
+						.AuthHeader("X-Api-Key", "john")
+						.AuthBasic("john", "lynch")
+						.Get(opts => { opts.Paginate(); })
+						.SetupAnonymousGet(opts => { opts.WriteString("Hello and welcome!"); })
+						.PostCreate(opts => {
+								opts.RequireProperty(p => p.PrivateKey)
+									.Default(p => p.FancyType, Guid.NewGuid)
+									.Default(p => p.Name, "Name")
+									.IncludePrimaryKey();
+						})
+						.GetByPrimaryKey()
+						.PostUpdateByPrimaryKey()
+						.DeleteByPrimaryKey();*/
+					options
+						.WriteXml()
+						.WriteResponseCount()
+						.StripArrayIfSingleResult()
+						.WrapResponse()
+						.CatchExceptions()
+						.ParseJson()
+						.Deprecate("Upgrade to the newest v2 deLUX version of this api today!")
+						.WriteVersion("400.thousand.build.5")
+						.SetupAnonymousGet(o => o.Limit(2))
+						.AuthQuery("key", "john")
+						.AuthHeader("X-Api-Key", "john")
+						.AuthBasic("john", "lynch")
+						.PostCreate(o => o
+							.ParseXmlAndJsonArrays()
+							.IgnorePrimaryKey()
+							.RequireProperty(p => p.PrivateKey)
+							.IncludePrimaryKey())
+						.GetByPrimaryKey()
+						.PostUpdateByPrimaryKey()
+						.DeleteByPrimaryKey()
+						.Get(o => o.Paginate());
+			});
+		}
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public void ConfigureServices(IServiceCollection services) {
+			services.AddDbContext<TestDbContext>(options => { options.UseInMemoryDatabase("test"); });
+		}
+	}
+}
---
+++ TestProject/TestComponents/TestModel.cs
@@ -0,0 +1,25 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="TestModel.cs" company="John Lynch">
+//   This file is licensed under the MIT license
+//   Copyright (c) 2020 John Lynch
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace TestProject.TestComponents {
+	using System;
+	using System.ComponentModel.DataAnnotations;
+	using System.ComponentModel.DataAnnotations.Schema;
+	using System.Xml.Serialization;
+
+	public class TestModel {
+		public Guid FancyType { get; set; } = Guid.NewGuid();
+
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string PrivateKey { get; set; }
+	}
+}
